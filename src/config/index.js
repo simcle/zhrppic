31199e0650c7a96f199e798905fe4bfae9ddd271
NendiSofiandy <nendisofiandy80@gmail.com>
2026-02-15 17:42:43 +0700

    update

--- src/config/index.js
+++ src/config/index.js
@@ -1,10 +1,10 @@
 import dotenv from 'dotenv'
 import path from 'node:path'
 
+dotenv.config()
 const NODE_ENV = process.env.NODE_ENV || 'development'; // Default ke development jika tidak didefinisikan
 const envFile = NODE_ENV === 'production' ? '.env.production' : '.env.development';
 
-console.log(envFile)
 dotenv.config({ path: path.resolve(process.cwd(), envFile) });
 
 const config = {
